55998f627042c0dd561790ea4d92ffa31fc3a87a
Krushil <krushilnaik96@gmail.com>
2021-02-28 09:45:58 -0500

    clean up imports and add more documentation

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 const { Manager } = require("./lib/Manager");
 const { Engineer } = require("./lib/Engineer");
 const { Intern } = require("./lib/Intern");
-const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
-
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
@@ -96,6 +93,10 @@ const internQuestions = [
 	},
 ];
 
+
+/**
+ * Call this to run through all the prompts
+ */
 async function init() {
 	console.log("Please build your team");
 
